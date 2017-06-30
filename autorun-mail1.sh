705c25f597e5d971ce966d2d42040d943547b904
Barbie <barbie@missbarbell.co.uk>
2017-06-30 12:48:14 +0000

    turn on verbose mode for logging

--- autorun-mail1.sh
+++ autorun-mail1.sh
@@ -23,28 +23,28 @@ else
 
     # run the daily reports
     echo `date +"$date_format"` "mode=daily" >>$LOG
-    perl bin/cpanreps-mailer --config=data/preferences-daily.ini >>$LOG 2>&1
+    perl bin/cpanreps-mailer --verbose --config=data/preferences-daily.ini >>$LOG 2>&1
 
     # run the named weekly reports
     day=`date +"%a"`
     echo `date +"$date_format"` "mode=$day" >>$LOG
-    perl bin/cpanreps-mailer --config=data/preferences-weekly.ini --mode=$day >>$LOG 2>&1
+    perl bin/cpanreps-mailer --verbose --config=data/preferences-weekly.ini --mode=$day >>$LOG 2>&1
 
     # run the generic weekly on a Saturday morning
     if [ `date +"%w"` -eq 6 ]; then
       echo `date +"$date_format"` "mode=weekly" >>$LOG
-      perl bin/cpanreps-mailer --config=data/preferences-weekly.ini >>$LOG 2>&1
+      perl bin/cpanreps-mailer --verbose --config=data/preferences-weekly.ini >>$LOG 2>&1
     fi
 
     # run the monthly on the first day of the month
     if [ `date +"%-d"` -eq 1 ]; then
       echo `date +"$date_format"` "mode=monthly" >>$LOG
-      perl bin/cpanreps-mailer --config=data/preferences.ini --mode=monthly --logfile=logs/monthly-mailer.log >>$LOG 2>&1
+      perl bin/cpanreps-mailer --verbose --config=data/preferences.ini --mode=monthly --logfile=logs/monthly-mailer.log >>$LOG 2>&1
     fi
 
     # produce the individual reports
     echo `date +"$date_format"` "mode=reports" >>$LOG
-    perl bin/cpanreps-mailer --config=data/preferences.ini --mode=reports --logfile=logs/reports-mailer.log >>$LOG 2>&1
+    perl bin/cpanreps-mailer --verbose --config=data/preferences.ini --mode=reports --logfile=logs/reports-mailer.log >>$LOG 2>&1
 
     echo `date +"$date_format"` "STOP" >>$LOG
 
